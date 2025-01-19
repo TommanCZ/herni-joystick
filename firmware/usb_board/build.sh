56b6ee43ede6a221b2f8b024351ec0046850ca7e
Lugaricci <tomas.partl.jr@email.cz>
2025-01-19 22:32:21 +0100

    updated the build.sh file to properly compile the v-usb library

--- firmware/usb_board/build.sh
+++ firmware/usb_board/build.sh
@@ -5,6 +5,7 @@ cd ./build
 avr-gcc -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/arduino -c ../lib/arduino/*.{c,S}
 avr-g++ -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/arduino -c ../lib/arduino/*.cpp
 avr-g++ -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/arduino -I../lib/nrflite -c ../lib/nrflite/NRFLite.cpp
-avr-g++ -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/arduino -I../lib/nrflite -I. -c ../src/main.cpp
+avr-gcc -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/usbdrv  -c ../lib/usbdrv/*.{c,S}
+avr-g++ -mmcu=atmega328p -DF_CPU=16000000UL -Os -I../lib/arduino -I../lib/nrflite -I../lib/usbdrv -I. -c ../src/main.cpp
 avr-g++ -mmcu=atmega328p -Os *.o -o main.elf
 avr-objcopy -O ihex -R .eeprom main.elf main.hex
